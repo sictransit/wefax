7640483c4841e4b503e5fee032bc0051b141dde1
Mikael Fredriksson <frimurare@gmail.com>
2020-07-12 11:23:39 +0200

    Add project files.

---
+++ Fax.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeFax
+{
+    class Fax
+    {
+        private const int Carrier = 1600;
+        private readonly int lineLength;
+        private readonly double dt;
+        private readonly int deviation;
+        private readonly int ioc;
+        
+        private double time = 0;
+
+        public Fax(int sampleRate = 8000, int deviation = 400, int ioc = 576)
+        {
+            SampleRate = sampleRate;
+            this.deviation = deviation;
+            this.ioc = ioc;
+            this.lineLength = sampleRate / 2;
+            this.dt = Math.PI * 2 / sampleRate;
+        }
+
+        public double Resolution => (Math.PI * ioc);
+
+        public int SampleRate { get; }
+
+        public float[] GetPhasing()
+        {
+            var white = lineLength / 20;            
+            var modulation = Enumerable.Range(0, lineLength).Select(x => x < white ? 1d : -1d).ToArray();
+
+            return Enumerable.Range(0, 20 * 2).Select(_ => GetLine(modulation)).SelectMany(x => x).ToArray();
+        }
+
+        public float[] GetStart()
+        {
+            return GetSquareWave(300, 5);
+        }
+
+        public float[] GetStop()
+        {
+            return GetSquareWave(450, 5);
+        }
+
+        public float[] GetBCH(BCH bch)
+        {
+            var white = Enumerable.Range(0, lineLength / 20).Select(_ => 1d).ToArray();
+
+            var bitLength = (lineLength - white.Length) / bch.Binary.Count() ;
+
+            var one = Enumerable.Repeat(1d, bitLength).ToArray();
+            var zero = Enumerable.Repeat(-1d, bitLength).ToArray();
+
+            var bin = bch.Binary.Select(x => x == 1 ? one : zero).SelectMany(x => x).ToArray();            
+
+            var padding = new double[lineLength - white.Length - bin.Length];
+
+            return Enumerable.Range(0, bitLength / 4).Select(_ => GetLine(white.Concat(bin).Concat(padding).ToArray())).SelectMany(x => x).ToArray();
+        }
+
+        private float[] GetSquareWave(int frequency, int duration)
+        {
+            var modulation = Enumerable.Range(0, frequency).Select(x => x % 2 == 0 ? -1d : 1d).ToArray();            
+
+            return Enumerable.Range(0, duration * 2).Select(_ => GetLine(modulation)).SelectMany(x => x).ToArray();
+        }
+
+        public float[] GetLine(double[] pixels = null)
+        {
+            //time = 0;
+
+            if (pixels == null)
+            {
+                pixels = new double[1];
+            }
+
+            var lineLength = SampleRate / 2;
+
+            var interpolationFactor = (double)pixels.Length / lineLength;
+
+            var line = new float[lineLength];
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                var pixel = (int)(i * interpolationFactor);                
+
+                var frequency = Carrier + deviation * pixels[pixel];
+
+                time += dt * frequency;
+
+                line[i] = (float)Math.Sin(time);                
+            }
+
+            return line;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using NAudio.Wave;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace WeFax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Send(@"C:\Users\micke\Dropbox\GC\!hidden\GC8WD6Q - WEFAX (bonus #3)\calibration.png", new BCH("GC8WD6Q", "MST", DateTime.UtcNow, "JO89", "d: 5102 m, b: 37.07 deg"));
+            Send(@"C:\Users\micke\Dropbox\GC\!hidden\GC8WD6Q - WEFAX (bonus #3)\map.png");
+        }
+
+        private static void Send(string filename, BCH bch = null)
+        {
+            var fax = new Fax(16000);
+
+            using var original = Bitmap.FromFile(filename);
+
+            var borderWidth = (int)fax.Resolution / 20;
+            var imageWidth = (int)fax.Resolution - borderWidth;
+
+            var scalingFactor = (fax.Resolution - borderWidth) / original.Width;
+
+            var faxWidth = borderWidth + imageWidth;
+            var faxHeight = original.Height * scalingFactor;
+
+            using var scaled = new Bitmap(faxWidth, (int)Math.Round(faxHeight));
+            using var graphics = Graphics.FromImage(scaled);
+            using var whiteBrush = new SolidBrush(Color.White);
+            graphics.FillRectangle(whiteBrush, 0, 0, scaled.Width, scaled.Height);
+
+            graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            graphics.DrawImage(original, (int)borderWidth, 0, (int)imageWidth, (int)faxHeight);
+
+            scaled.Save("scaled.png", ImageFormat.Png);
+
+            var wavFilename = Path.Combine(Path.GetDirectoryName(filename), $"{Path.GetFileNameWithoutExtension(filename)}.wav");
+
+            using var writer = new WaveFileWriter(wavFilename, new WaveFormat(fax.SampleRate, 1));
+
+            var start = fax.GetStart();
+
+            writer.WriteSamples(start, 0, start.Length);
+
+            var phasing = fax.GetPhasing();
+
+            writer.WriteSamples(phasing, 0, phasing.Length);
+
+            if (bch != null)
+            {
+                var header = fax.GetBCH(bch);
+
+                writer.WriteSamples(header, 0, header.Length);
+            }
+
+            for (int y = 0; y < scaled.Height; y++)
+            {
+                var pixels = Enumerable.Range(0, scaled.Width).Select(x => scaled.GetPixel(x, y).GetBrightness() * 2d - 1).ToArray();
+
+                var samples = fax.GetLine(pixels);
+
+                writer.WriteSamples(samples, 0, samples.Length);
+            }
+
+            var stop = fax.GetStop();
+
+            writer.WriteSamples(stop, 0, stop.Length);
+
+            Console.WriteLine($"{scaled.Width}x{scaled.Height}");
+        }
+    }
+}
